bd5a031c7a8bcab2ee002c184eba41d1d714f9d6
Alyssa Maquiling <alymaquiling@gmail.com>
2019-10-26 13:41:23 -0400

    first commit

---
+++ src/components/TermFreqGraphPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import JetBlueNavBar from './JetBlueNavBar';
+
+class TermFreqGraphPage extends Component {
+    render() {
+        return <h1>TF page</h1>;
+    }
+}
+
+export default TermFreqGraphPage;
